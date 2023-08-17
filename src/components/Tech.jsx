06039bb248029d4214d9526c4f721802bf31eb11
arpit <arpit.2018.btech.jiit@gmail.com>
2023-08-17 13:41:00 +0530

    Resolved the scrolling issue

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -27,13 +27,6 @@ const Tech = () => {
                 <BallCanvas icons={icons} />
             </div>
         </>
-        /*<div className='flex flex-row flex-wrap justify-center gap-10'>
-            {technologies.map((technology) => (
-                <div className='w-28 h-28' key={technology.name}>
-                    <BallCanvas icon={technology.icon} />
-                </div>
-            ))}
-        </div>*/
     );
 };
 
--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -9,10 +9,9 @@ import {
 } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
-
 import * as THREE from 'three';
 
-const Ball = ({ icon, position, rotation }) => {
+const Ball = ({ icon, position }) => {
     const decal = useMemo(() => new THREE.TextureLoader().load(icon), [icon]);
 
     return (
@@ -46,7 +45,7 @@ const BallCanvas = ({ icons }) => {
             camera={{ position: [0, 0, 5], fov: 75 }}
             gl={{ preserveDrawingBuffer: true }}>
             <Suspense fallback={<CanvasLoader />}>
-                <OrbitControls enableZoom={false} />
+                <OrbitControls enableZoom={false} enabled={false} />
                 <group>
                     {icons.map((icon, index) => {
                         const row = Math.floor(index / 3);
@@ -65,46 +64,4 @@ const BallCanvas = ({ icons }) => {
 
 export default BallCanvas;
 
-/*const Ball = (props) => {
-  const [decal] = useTexture([props.imgUrl]);
 
-  return (
-    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
-      <ambientLight intensity={0.25} />
-      <directionalLight position={[0, 0, 0.05]} />
-      <mesh castShadow receiveShadow scale={2.75}>
-        <icosahedronGeometry args={[1, 1]} />
-        <meshStandardMaterial
-          color='#fff8eb'
-          polygonOffset
-          polygonOffsetFactor={-5}
-          flatShading
-        />
-        <Decal
-          position={[0, 0, 1]}
-          rotation={[2 * Math.PI, 0, 6.25]}
-          scale={1}
-          map={decal}
-          flatShading
-        />
-      </mesh>
-    </Float>
-  );
-};*/
-
-/*const BallCanvas = ({ icon }) => {
-  return (
-    <Canvas
-      frameloop='demand'
-      dpr={[1, 2]}
-      gl={{ preserveDrawingBuffer: true }}
-    >
-      <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls enableZoom={false} />
-        <Ball imgUrl={icon} />
-      </Suspense>
-
-      <Preload all />
-    </Canvas>
-  );
-};*/
